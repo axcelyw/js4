3817f091ed70b9dc2c4447d72f220cfacae103a1
ramis <axcelyw@gmail.com>
2022-09-28 19:21:11 +0500

    final

---
+++ script.js
@@ -0,0 +1,41 @@
+let name = prompt ('Введите свое имя:')
+let age = +prompt('Введите год рождения:')
+let thisyear = +prompt('Введите нынешний год:')
+
+function min (thisyear, year){
+    return thisyear - year; 
+}
+
+
+let result = min(thisyear, year);
+alert(name + ' ваш возраст ' + result) 
+
+
+
+
+
+
+
+
+function random (min, max) {
+    return Math.floor(Math.random() * (max, min +1) + min)
+}
+
+let minNum = +prompt('Введите мин число:')
+let maxNum = +prompt('Введите макс число:')
+
+for (let i=0; i <10; i++) {
+    let num1 = random(minNum, maxNum)
+    let num2 = random(minNum, maxNum)
+
+    let pr =+prompt(num1 + ' + ' + num2)
+    let pr1 =+prompt(num1 + ' - ' + num2)
+    let pr2 =+prompt(num1 + ' * ' + num2)
+    let pr3 =+prompt(num1 + ' / ' + num2)
+
+    console.log((num1 + num2) == pr ? 'Молодец верно' : 'Ваш ответ ' + pr + ' . Верный ответ ' + (num1 + num2));
+    console.log((num1 - num2) == pr ? 'Молодец верно' : 'Ваш ответ ' + pr + ' . Верный ответ ' + (num1 - num2));
+    console.log((num1 * num2) == pr ? 'Молодец верно' : 'Ваш ответ ' + pr + ' . Верный ответ ' + (num1 * num2));
+    console.log((num1 / num2) == pr ? 'Молодец верно' : 'Ваш ответ ' + pr + ' . Верный ответ ' + (num1 / num2));
+    
+}
